1bd4be5eb8fc18e6239cfdb652ae3a50e448e7e5
James.Zheng <James.Zheng@homecredit.cn>
2019-12-04 13:36:33 +0800

    first commit

---
+++ src/test/java/cn/james/sandbox/springamqpsandbox/SpringAmqpSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.james.sandbox.springamqpsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAmqpSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
